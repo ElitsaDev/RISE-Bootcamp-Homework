54b61f169491c6241bf6709fa4cab8cb5bb6a749
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-02-24 12:32:08 +0200

    refactoring

--- src/js/util.js
+++ src/js/util.js
@@ -1,11 +1,7 @@
 
-
-
-export default function FindPoint(xCity, yCity, xPoint, yPoint) {
-
+export default function checkPointPosition(xCity, yCity, xPoint, yPoint) {
     if (xPoint >= 0 && xPoint < xCity  && yPoint >= 0 && yPoint < yCity ) {
         return true;
     }
-
     return false;
 }
